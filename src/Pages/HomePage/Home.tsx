9ae0beb005f480e7b3943ad3e5faea0407be4c90
Pawel Mendzik <mendzik.pawel@gmail.com>
2023-04-11 19:44:25 +0200

    Working on pages

--- src/Pages/HomePage/Home.tsx
+++ src/Pages/HomePage/Home.tsx
@@ -5,50 +5,32 @@ import './Home.css'
 
 export const HomePage = () => {
 
+    const items:{url:string, alt:string, bgpos?:string}[] = [
+        {url:"https://cdnb.artstation.com/p/assets/images/images/061/504/475/large/pablossky-fg2.jpg?1680965487", alt:"Music Production", bgpos:"center"},
+        {url:"https://cdnb.artstation.com/p/assets/images/images/061/511/853/large/pablossky-bez-nazwy.jpg?1680981186", alt:"Arts", bgpos:"top"},
+        {url:"https://cdnb.artstation.com/p/assets/images/images/061/612/063/large/pablossky-workinprogress.jpg?1681229266", alt:"Blender", bgpos:"center"},
+        {url:"https://photos.google.com/photo/AF1QipM9Nq4NvKeCAny44C06a4kCIVOFPmNKuV-z1FOu", alt:"Coding", bgpos:"center"},
+     ];
+     
     return (
-        <Container className="headHome"> 
-            <h1>Hi from homepage!</h1>
-            <Carousel>
-        <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0c/Lighthouse_Rubjerg_Knude%2C_Denmark%2C_2004_ubt.jpeg"
-            alt="First slide"
-            />
-            <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src="images.musicicon"
-            alt="Second slide"
-            />
-
-            <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-            <img
-            className="d-block w-100"
-            src="../../Assets/img/articon.png"
-            alt="Third slide"
-            />
-
-            <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        </Carousel>
+         <>
+        <Carousel variant="dark" interval={5000}>
+            {items.map((items)=>{
+                return (<Carousel.Item className="slideshow">
+                <div
+                style={{backgroundImage: "url("+items.url+")", 
+                        backgroundPosition: items.bgpos ? items.bgpos : "center"}}
+                ></div>
+                <Carousel.Caption>
+                <h3>{items.alt}</h3>
+                </Carousel.Caption>
+            </Carousel.Item>)
+            })}
+            
+        </Carousel><Container className="headHome">
+        <Container className="textHome">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit illo molestias iure eos enim ratione, eius unde sint iusto, harum sunt odio velit tenetur magni nisi maxime corporis? Vero, cupiditate. Vitae, aliquid facilis laudantium repellat fugit rem saepe minus illum omnis ipsam doloremque modi voluptatem nihil ut fugiat quos perferendis fuga. Molestias quos aspernatur at sit dolorum soluta ullam harum! Ducimus, sapiente ab commodi, aspernatur rerum perspiciatis quis maxime, esse minus soluta non repellendus officia corrupti! Corporis sapiente aut hic consequatur, omnis enim, non nam corrupti officiis fugiat molestiae quam. Nesciunt totam voluptatum illo, odit at sint ipsa labore facilis amet quam eos? Error molestiae, tempora similique voluptatum illum voluptatem, dolorem doloremque, nostrum cumque est tempore consequatur cupiditate eum fugit. Voluptatibus dicta totam consectetur fuga qui modi, id rem ducimus sit labore fugiat natus odit suscipit consequuntur laboriosam doloribus illum quas dolor veritatis asperiores.
         </Container>
-
-        
+        </Container></>
     )
 }
