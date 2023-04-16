d74c2628940bd972ec5a7cc1681d06c726d51cfa
Pawel Mendzik <mendzik.pawel@gmail.com>
2023-04-16 03:19:19 +0200

    Tags

--- src/Pages/GalleryPage/Gallery.tsx
+++ src/Pages/GalleryPage/Gallery.tsx
@@ -1,30 +1,56 @@
 import { Masonry } from 'react-plock';
 import { Container } from "react-bootstrap"
+import { useState } from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 import './Gallery.css'
 
 export const GalleryPage = () => {
-    const items:{url:string, alt:string}[] = [
-       {url:"https://cdna.artstation.com/p/assets/images/images/061/509/010/large/pablossky-portrait4.jpg?1680975077", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/511/915/large/pablossky-0030.jpg?1680981372", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/511/853/large/pablossky-bez-nazwy.jpg?1680981186", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/508/907/large/pablossky-1.jpg?1680974910", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/508/869/large/pablossky-italy.jpg?1680974807", alt:""},
-       {url:"https://cdna.artstation.com/p/assets/images/images/061/508/804/large/pablossky-portrait-3.jpg?1680974658", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/508/797/large/pablossky-portrait-2.jpg?1680974651", alt:""},
-       {url:"https://cdna.artstation.com/p/assets/images/images/061/508/794/large/pablossky-portrait-1.jpg?1680974639", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/456/197/large/pablossky-av.jpg?1680826790", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/449/859/large/pablossky-sand.jpg?1680809648", alt:""},
-       {url:"https://cdna.artstation.com/p/assets/images/images/061/449/814/large/pablossky-ddart1.jpg?1680809585", alt:""},
-       {url:"https://cdna.artstation.com/p/assets/images/images/048/238/370/large/pablossky-img-20220409-024427.jpg?1649542779", alt:""},
-       {url:"https://cdnb.artstation.com/p/assets/images/images/061/504/475/large/pablossky-fg2.jpg?1680965487", alt:""},
-       {url:"https://i.imgur.com/VzA023r.png", alt:""},
-       {url:"https://i.imgur.com/TdaWklR.png", alt:""},
-       {url:"https://i.imgur.com/qJdlv6X.png", alt:""},
-       {url:"https://i.imgur.com/2AktXUT.jpg", alt:""},
-       {url:"https://i.imgur.com/VvYaLwt.png", alt:""},
+    const items:{url:string, alt:string, tag?:string,}[] = [
+       {url:"https://cdna.artstation.com/p/assets/images/images/061/509/010/large/pablossky-portrait4.jpg?1680975077", alt:"", tag:"Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/511/915/large/pablossky-0030.jpg?1680981372", alt:"", tag:"Blender Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/511/853/large/pablossky-bez-nazwy.jpg?1680981186", alt:"", tag:"Blender Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/508/907/large/pablossky-1.jpg?1680974910", alt:"", tag:"Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/508/869/large/pablossky-italy.jpg?1680974807", alt:"", tag:"Digital"},
+       {url:"https://cdna.artstation.com/p/assets/images/images/061/508/804/large/pablossky-portrait-3.jpg?1680974658", alt:"", tag:"Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/508/797/large/pablossky-portrait-2.jpg?1680974651", alt:"", tag:"Digital"},
+       {url:"https://cdna.artstation.com/p/assets/images/images/061/508/794/large/pablossky-portrait-1.jpg?1680974639", alt:"", tag:"Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/456/197/large/pablossky-av.jpg?1680826790", alt:"", tag:"Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/449/859/large/pablossky-sand.jpg?1680809648", alt:"", tag:"Digital"},
+       {url:"https://cdna.artstation.com/p/assets/images/images/061/449/814/large/pablossky-ddart1.jpg?1680809585", alt:"", tag:"Digital"},
+       {url:"https://cdna.artstation.com/p/assets/images/images/048/238/370/large/pablossky-img-20220409-024427.jpg?1649542779", alt:"", tag:"Blender Digital"},
+       {url:"https://cdnb.artstation.com/p/assets/images/images/061/504/475/large/pablossky-fg2.jpg?1680965487", alt:"", tag:"Digital"},
+       {url:"https://i.imgur.com/VzA023r.png", alt:"", tag:"Blender Digital"},
+       {url:"https://i.imgur.com/TdaWklR.png", alt:"", tag:"Blender Digital"},
+       {url:"https://i.imgur.com/qJdlv6X.png", alt:"", tag:"Blender Digital"},
+       {url:"https://i.imgur.com/2AktXUT.jpg", alt:"", tag:"Traditional"},
+       {url:"https://i.imgur.com/VvYaLwt.png", alt:"", tag:"Digital"},
     ];
 
+    const [filteredItems, setFilteredItems]= useState(items);
+    const [radioValue, setRadioValue] = useState('All');
+
+    const radios = [
+        { name: 'All', value: 'All' },
+        { name: 'Blender', value: 'Blender' },
+        { name: 'Digital Art', value: 'Digital' },
+        { name: 'Traditional', value: 'Traditional' },
+      ];
+
+    function filterList(value:string) {
+        setRadioValue (value);
+
+        let data:any[]=[];
+
+        if (value=="All"){data=items;}
+        else {
+            data = items.filter(items=>items.tag?.includes(value));
+        }
+        setFilteredItems(data);
+    }
+
+    
     return <>
         <Container className="headGallery">
             <h1>My arts</h1>
@@ -34,10 +60,29 @@ export const GalleryPage = () => {
             </article>
         </Container>
 
-        <Container>
+        <Container className="gallery">
+            <div className='galleryButtons'>
+            <ButtonGroup>
+                {radios.map((radio, idx) => (
+                  <ToggleButton
+                    className='radioButton'
+                    key={idx}
+                    id={`radio-${idx}`}
+                    type="radio"
+                    variant={'dark'}
+                    name="radio"
+                    value={radio.value}
+                    checked={radioValue === radio.value}
+                    onChange={(e) => filterList(e.currentTarget.value)}
+                  >
+                    {radio.name}
+                  </ToggleButton>
+                ))}
+            </ButtonGroup>
+            </div>
         <Masonry
             className='galery-masonry'
-            items={items}
+            items={filteredItems}
             config={{
                 columns: [1, 2, 3],
                 gap: [24, 12, 6],
